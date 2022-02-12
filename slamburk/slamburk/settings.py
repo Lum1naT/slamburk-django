6a0f1c50dceb9777a719f16b4c8ffe11263870c0
Lum1naT <47953544+Lum1naT@users.noreply.github.com>
2022-02-12 10:37:06 +0100

    ?

--- slamburk/slamburk/settings.py
+++ slamburk/slamburk/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
+    'main.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +82,13 @@ WSGI_APPLICATION = 'slamburk.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': str(os.environ.get("POSTGRES_DB")),
+        'USER': str(os.environ.get("POSTGRES_USER")),
+        'PASSWORD': str(os.environ.get("POSTGRES_PASSWORD")),
+        'HOST': str(os.environ.get("POSTGRES_HOST")),
+        'PORT': '',
+        # postgres://tpyilmgs:d9M0crG6lJO7B4ZwB_2bSTr_SyUEeSGr@abul.db.elephantsql.com/tpyilmgs
     }
 }
 
